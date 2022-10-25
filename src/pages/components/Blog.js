9d58a6ca28f2cbd8bf672681b4e48450e9fa216b
rubayetseason <season.ss2001@gmail.com>
2022-10-25 22:28:19 +0600

    blog done

--- src/pages/components/Blog.js
+++ src/pages/components/Blog.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
   return (
-    <div>
-      this is blog
-    </div>
+    <Container>
+      <h2 className='mt-5 bg-danger text-white py-2'>General questions</h2>
+      <div className='mt-4'>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>What is CORS?</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Why are we using Firebase? What other options there are to implement authentication?</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience. When the device regains connection, the Realtime Database synchronizes the local data changes with the remote updates that occurred while the client was offline, merging any conflicts automatically. The Realtime Database provides a flexible, expression-based rules language, called Firebase Realtime Database Security Rules, to define how your data should be structured and when data can be read from or written to. When integrated with Firebase Authentication, developers can define who has access to what data, and how they can access it.
+                <br />
+                <br />
+                Other Firebase alternatives are Back4App, Backendless, Kuzzle, Kumulos, Deployd, NHost.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>How does the private route work?</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                The react private route component renders child components (children) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property. The current logged in user (authUser) is retrieved from Redux state with a call to the useSelector() hook. Redux is used in this example however it is not required to implement a Private Route component in React Router 6. You could use a different state management library or any approach you prefer to get the logged in status of the user.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive. Working of Node.js: Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request. <br />
+                Node.js basically works on two concept
+                <br />
+                1. Asynchronous <br />
+                2. Non-blocking I/O
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    </Container>
+
   );
 };
 
